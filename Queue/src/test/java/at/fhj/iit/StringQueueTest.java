d17f4610831364922e6e3945c55c9b2a278d036a
flokosti <florian.kostmann@edu.fh-joanneum.at>
2019-06-12 12:02:17 +0200

    Added testIsEmpty and testAddElement testcases

--- Queue/src/test/java/at/fhj/iit/StringQueueTest.java
+++ Queue/src/test/java/at/fhj/iit/StringQueueTest.java
@@ -2,27 +2,33 @@ package at.fhj.iit;
 
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
+
 public class StringQueueTest {
 
-
-    /**
-     * TODO Write your own tests
-     */
-
+    public StringQueue q;
     @Before
     public void setUp() throws Exception {
+        q=new StringQueue(5);
+    }
 
+    @Test (expected = NoSuchElementException.class)
+    public void testIsEmpty() {
+        q.element();
     }
 
     @Test
-    public void test1() {
-    }
+    public void testAddElements() {
+        Assert.assertTrue(q.offer("Obj1"));
+        Assert.assertTrue(q.offer("Obj2"));
+        Assert.assertTrue(q.offer("Obj3"));
+        Assert.assertTrue(q.offer("Obj4"));
+        Assert.assertTrue(q.offer("Obj5"));
 
-    @Test
-    public void test2() {
     }
 
     @Test
