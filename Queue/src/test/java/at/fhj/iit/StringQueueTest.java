3595b99a23f8e7bf039c7b8c8eeec91678384f21
Leopold Wurm <leopold.wurm@edu.fh-joanneum.at>
2019-06-12 11:54:37 +0200

    Added empty  test methods

--- Queue/src/test/java/at/fhj/iit/StringQueueTest.java
+++ Queue/src/test/java/at/fhj/iit/StringQueueTest.java
@@ -18,7 +18,43 @@ public class StringQueueTest {
     }
 
     @Test
-    public void test() {
+    public void test1() {
+    }
+
+    @Test
+    public void test2() {
+    }
+
+    @Test
+    public void test3() {
+    }
+
+    @Test
+    public void test4() {
+    }
+
+    @Test
+    public void test5() {
+    }
+
+    @Test
+    public void test6() {
+    }
+
+    @Test
+    public void test7() {
+    }
+
+    @Test
+    public void test8() {
+    }
+
+    @Test
+    public void test9() {
+    }
+
+    @Test
+    public void test10() {
     }
 
     @After
